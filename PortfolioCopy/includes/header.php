ca7685b4621dc0494c90792c80e59fba66462685
Lucas Lübbers <146840701+LucasLubbers@users.noreply.github.com>
2024-01-18 16:29:30 +0100

    Uploaded files

---
+++ PortfolioCopy/includes/header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="./css/includes/header.css">
+<div class="headerContainer">
+    <div class="headerLeft">
+        <p>Portfolio Lucas Lübbers</p>
+    </div>
+    <div class="headerRight">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="professional.php">Professional Skills</a></li>
+            <!-- <li><a href="projecten.php">Projecten</a></li> -->
+            <!-- <li><a href="#">Overig</a></li> -->
+        </ul>
+    </div>
+</div>
